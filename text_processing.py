09d84f0e80c6350c705767f4bc21d9753404502e
entrekid <dat.sci.seol@gmail.com>
2021-01-19 13:18:28 +0900

    assignment done

--- text_processing.py
+++ text_processing.py
@@ -33,7 +33,7 @@ def normalize(input_string):
              >>> tp.normalize(input_string2)
              'extra space'
     """
-    normalized_string = None
+    normalized_string = input_string.lower().strip()
     return normalized_string
 
 
@@ -58,5 +58,11 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
-    no_vowel_string = None
+    no_vowel_string = input_string.replace("a", "").replace(
+        "e", "").replace("i", "").replace("o", "").replace("u", "")
     return no_vowel_string
+
+
+if __name__ == "__main__":
+    print(normalize("  good  "))
+    print(no_vowels("hello"))
